b646182a25fb9eb64153a0370d3a4fe790e5c8ab
Matthew Hartley <mhartley@cantab.net>
2020-03-31 09:36:06 +0100

    Update for new library function compatability

--- scripts/generate_spherefit_visualisations.py
+++ scripts/generate_spherefit_visualisations.py
@@ -1,31 +1,34 @@
+import os
 import click
 
 import numpy as np
 from dtoolbioimage import Image
 
 from stacktools.data import FCADataSetLoader, FCARootData
-from stacktools.vis import centroid_project, generate_annotated_file_projection
-
+from stacktools.vis import centroid_project, create_annotated_file_projection
 
 
 @click.command()
 @click.argument('image_ds_uri')
 @click.argument('seg_ds_uri')
 @click.argument('root_data_dirpath')
+@click.argument('output_base_dirpath')
 @click.option('--root-name', default="fca3_FLCVenus_root2")
-def main(image_ds_uri, seg_ds_uri, root_data_dirpath, root_name):
+def main(image_ds_uri, seg_ds_uri, root_data_dirpath, output_base_dirpath, root_name):
 
     loader = FCADataSetLoader(image_ds_uri, seg_ds_uri, root_data_dirpath)
     rootdata = loader.load_root(root_name)
 
     sections = [
-        generate_annotated_file_projection(rootdata, fid)
+        create_annotated_file_projection(rootdata, fid, rootdata.denoised_venus_stack)
         for fid in rootdata.files
     ]
 
     composite = np.vstack(sections).view(Image)
 
-    composite.save(f"{root_name}-composite.png")
+    fname = f"{root_name}-spherefit-allfiles.png"
+    fpath = os.path.join(output_base_dirpath, fname)
+    composite.save(fpath)
 
 
 if __name__ == "__main__":
